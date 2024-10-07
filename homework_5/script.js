478bd95dac6191cfd25a493003afe501695a9046
Marina Kokorovets <mkokorovetz@gmail.com>
2024-10-07 11:15:03 +0300

    5.1

--- homework_5/script.js
+++ homework_5/script.js
@@ -1,23 +1,19 @@
-function clickOK(){
-	event.preventDefault()
-    let f = document.getElementsByName('field');
-    let r = document.getElementById('result');
-    let s = document.getElementsByName('select');
-    if (f[0].value.match(/^\d+$/) && f[0].value >= 0 ) {
-			r.innerHTML =
-				'Стоимость товара: ' +
-				s[0].value +
-				', итоговая сумма: ' +
-				f[0].value * s[0].value
-		} else {
-			alert('Введите целое положительное число без других символов')
-			r.innerHTML = ''
-		}
-    return false;
+function clickOK(event) {
+    event.preventDefault();
+    let f = document.getElementsByName("field");
+    let r = document.getElementById("result");
+    let s = document.getElementsByName("select");
+    if (f[0].value.match(/^\d+$/) && f[0].value >= 0) {
+        r.innerHTML = "Стоимость товара: " + s[0].value + ", итоговая сумма: "
+        + f[0].value * s[0].value;
+    } else {
+        alert("Введите целое положительное число без других символов");
+        r.innerHTML = "";
+    }
 }
 
-window.addEventListener('DOMContentLoaded', function (event){
-	console.log("DOM is Ready");
-	let b = document.getElementById("butt");
-	b.addEventListener('click', clickOK);
+window.addEventListener("DOMContentLoaded", function () {
+    console.log("DOM is Ready");
+    let b = document.getElementById("butt");
+    b.addEventListener("click", clickOK);
 });
